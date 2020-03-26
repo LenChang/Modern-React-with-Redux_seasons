5f7462af6fc3c7c0b6ae5fff95e0fffaf3062bf7
Len Chang <lamgo.chang@gmail.com>
2020-03-26 16:51:28 +0800

    63 to 67 Modern React with Redux - Passing State as Props - Determining Season - Ternary Expressions in JSX - Showing icons - Extracting Options to Config Objects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import SeasonDisplay from "./SeasonDisplay";
+
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
 
@@ -25,7 +27,7 @@ class App extends React.Component {
     }
 
     if (this.state.lat && !this.state.errorMessage) {
-      return <div>Latitude:{this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading</div>;
